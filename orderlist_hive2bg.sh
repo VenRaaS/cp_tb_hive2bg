b90d175efd91ec865dab0534c12982adb191a43b
ROBIN SU <robin.rsu@gmail.com>
2016-12-05 15:54:58 +0800

    Create orderlist_hive2bg.sh

---
+++ orderlist_hive2bg.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+export EX_DATE=$(date --date="${1-yesterday}" +%Y%m%d)
+export EX_DATE10=$(date --date="${1-yesterday}" +%Y-%m-%d)
+export TABLE_NM="orderlist_"${EX_DATE}
+export HIVE_TMP_DB="itri"
+export BQ_TMP_DB='itri'
+export BQ_DB='nono_Unima'
+export HDFS_TMP_DIR='/user/itri'/${TABLE_NM}
+export GS_TMP_DIR='gs://venraastest-bq-upload'/${TABLE_NM}
+export GCP_PROJECT_ID='venraas-test'
+
+# clean up
+echo "=>delete local temp folder ./${TABLE_NM}"
+rm -r ./${TABLE_NM}/
+echo "=>drop Hive temp table ${HIVE_TMP_DB}.${TABLE_NM}"
+hive -e "drop table ${HIVE_TMP_DB}.${TABLE_NM}"
+echo "=>remove gs temp folder ${GS_TMP_DIR}"
+gsutil rm -r  ${GS_TMP_DIR}
+echo "=>drop bq temp table ${BQ_TMP_DB}.${TABLE_NM}"
+bq rm --project_id ${GCP_PROJECT_ID}t -f -t ${BQ_TMP_DB}.${TABLE_NM}
+echo "=>drop bq table ${BQ_DB}.${TABLE_NM}"
+bq rm --project_id ${GCP_PROJECT_ID} -f -t ${BQ_DB}.${TABLE_NM}
+
+#begin
+echo "create Hive table -${HIVE_TMP_DB}.${TABLE_NM}"
+hive -e "create table ${HIVE_TMP_DB}.${TABLE_NM}
+    STORED AS AVRO
+    LOCATION '${HDFS_TMP_DIR}'
+    AS
+    select uid,order_no,seq,unix_timestamp(order_date) as order_date,gid,currency,sale_price,final_price,qty,final_amt,promo_id,affiliate_id,dc_price,delivery_type,unix_timestamp(update_time) as update_time
+    from angel.all_orderlist
+    where to_date(order_date) = from_unixtime(unix_timestamp('${EX_DATE}' ,'yyyyMMdd'), 'yyyy-MM-dd')"
+# get file from hdfs
+echo "=>get hdfs files ${HDFS_TMP_DIR} to ./${TABLE_NM}"
+hadoop fs -get ${HDFS_TMP_DIR} .
+# upload to gs
+echo "=>upload file to gs ${GS_TMP_DIR}"
+gsutil cp ./${TABLE_NM}/* ${GS_TMP_DIR}
+echo "=>create bq temp table - ${BQ_TMP_DB}.${TABLE_NM}- with external gs source - ${GS_TMP_DIR}/*"
+bq load --project_id ${GCP_PROJECT_ID} --source_format=AVRO  ${BQ_TMP_DB}.${TABLE_NM} ${GS_TMP_DIR}/*
+echo "=>create bq table ${BQ_DB}.${TABLE_NM}"
+bq query --project_id ${GCP_PROJECT_ID} --nouse_legacy_sql --allow_large_results --destination_table=${BQ_DB}.${TABLE_NM} "select uid,order_no,seq,DATETIME(TIMESTAMP_SECONDS(order_date), '+08:00') as order_date,gid,currency,sale_price,final_price,qty,final_amt,promo_id,affiliate_id,dc_price,delivery_type,DATETIME(TIMESTAMP_SECONDS(update_time), '+08:00') as update_time FROM ${BQ_TMP_DB}.${TABLE_NM}"
+
+# clean up
+echo "=>delete local temp folder ./${TABLE_NM}"
+rm -r ./${TABLE_NM}/
+echo "=>drop Hive temp table ${HIVE_TMP_DB}.${TABLE_NM}"
+hive -e "drop table ${HIVE_TMP_DB}.${TABLE_NM}"
+echo "=>remove gs temp folder ${GS_TMP_DIR}"
+gsutil rm -r  ${GS_TMP_DIR}
+echo "=>drop bq temp table ${BQ_TMP_DB}.${TABLE_NM}"
+bq rm --project_id ${GCP_PROJECT_ID}t -f -t ${BQ_TMP_DB}.${TABLE_NM}
+echo "=>drop bq table ${BQ_DB}.${TABLE_NM}"
+bq rm --project_id ${GCP_PROJECT_ID} -f -t ${BQ_DB}.${TABLE_NM}
