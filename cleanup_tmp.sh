636f0293f9f8b60e9e20c57a1445ee90907a3d2b
ROBIN SU <robin.rsu@gmail.com>
2016-12-06 10:24:59 +0800

    Update cleanup_tmp.sh

--- cleanup_tmp.sh
+++ cleanup_tmp.sh
@@ -9,5 +9,3 @@ echo "=>remove gs temp folder ${GS_TMP_DIR}"
 gsutil rm -r  ${GS_TMP_DIR}
 echo "=>drop bq temp table ${BQ_TMP_DB}.${TABLE_NM}"
 bq rm --project_id ${GCP_PROJECT_ID}t -f -t ${BQ_TMP_DB}.${TABLE_NM}
-echo "=>drop bq table ${BQ_DB}.${TABLE_NM}"
-bq rm --project_id ${GCP_PROJECT_ID} -f -t ${BQ_DB}.${TABLE_NM}
