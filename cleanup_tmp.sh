1009aee4e78f7c6060d3783120199c02299e6f06
ROBIN SU <robin.rsu@gmail.com>
2016-12-06 10:34:52 +0800

    Update cleanup_tmp.sh

--- cleanup_tmp.sh
+++ cleanup_tmp.sh
@@ -6,6 +6,6 @@ rm -r ./${TABLE_NM}/
 echo "=>drop Hive temp table ${HIVE_TMP_DB}.${TABLE_NM}"
 hive -e "drop table ${HIVE_TMP_DB}.${TABLE_NM}"
 echo "=>remove gs temp folder ${GS_TMP_DIR}"
-gsutil rm -r  ${GS_TMP_DIR}
+gsutil -m rm -r  ${GS_TMP_DIR}
 echo "=>drop bq temp table ${BQ_TMP_DB}.${TABLE_NM}"
 bq rm --project_id ${GCP_PROJECT_ID}t -f -t ${BQ_TMP_DB}.${TABLE_NM}
